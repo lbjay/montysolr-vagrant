93d601e89a5ebdefd56b073c4506364bb00a688d
Jay Luker <jluker@cfa.harvard.edu>
2014-01-07 16:00:16 -0500

    forgot to add automake

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,6 @@ aptitude update
 aptitude install -y git vim wget openjdk-7-jdk ant screen python-virtualenv libmysqlclient-dev gcc \
  libmysqlclient-dev libxml2-dev python-dev python-libxml2 libxslt-dev subversion g++ \
  gettext mysql-client netpbm pstotext giflib-tools html2text pdftk gs-common gnuplot \
- apache2-mpm-prefork mysql-server poppler-utils clisp libapache2-mod-wsgi unzip
+ apache2-mpm-prefork mysql-server poppler-utils clisp libapache2-mod-wsgi unzip automake
 
 . /var/lib/montysolr/run-perpetuum-check.sh
